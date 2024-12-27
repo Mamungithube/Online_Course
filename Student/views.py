80396ad02a0d5600f5d626ba20cd2e8c9950b137
mamungithube <orroshidmdmamun50@gmail.com>
2024-12-27 17:02:40 +0600

    done

--- Student/views.py
+++ Student/views.py
@@ -65,9 +65,9 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect("http://127.0.0.1:8000/Student/login/")
+        return redirect("http://127.0.0.1:5500/login.html")
     else:
-        return redirect("http://127.0.0.1:8000/Student/register/")
+        return redirect("http://127.0.0.1:5500/registetion.html")
 
 
 class UserLoginApiView(APIView):
